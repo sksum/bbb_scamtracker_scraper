03717aefa60c751c24de6cb0a2f168059a0c6162
Saksham Mrig <sakshammrigdgr8@gmail.com>
2021-04-23 14:15:14 +0530

    analysis done

---
+++ analysis/analysis3.0/README.md
@@ -0,0 +1,48 @@
+## Analysis 3.0
+
+#### Minutes of Meeting [19 March 2021]
+
+
+Check for “I received” keyword in scam dataset to make different categories.
+- Email
+- Text message
+- Facebook
+- Phone call/ call/called me up
+- Social media
+- Instagram
+- Twitter
+- LinkedIn
+- Personal message
+- Whatsapp
+- Message
+- Telegram 
+- Phone message
+- Contact number
+
+
+
+
+
+
+### Tree structure:
+
+
+
+				Jobs [2015-2021]
+
+
+
+Email 			Call			Social media			Text message
+
+
+
+
+---Work from Home 	--Wfh		
+
+And its variants
+
+---xyz
+
+--wgf
+
+
---
+++ analysis/analysis3.0/b2.cpp
@@ -0,0 +1,113 @@
+/**
+ * author: Bhavesh
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<str> vs;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define cinarr(ar, n)           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> ar[i];           \
+    }
+#define car(ar, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << ar[i] << " ";   \
+    }
+#define forn(i, a, b) for (int i = (a); i < ll(b); ++i)
+#define fo(i, a) forn(i, 0, ll(a))
+#define rforn(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define ro(i, a) rforn(i, 0, a)
+#define trav(a, x) for (auto &a : x)
+
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define sec second
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+const ll INF = 1e18;
+
+int main()
+{
+    int n,B;
+    cin >> n>>B;
+    int a[n],b[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i]>>b[i];
+    }
+    priority_queue<pi> pinv;
+    
+    for (int i = 0; i < n; i++)
+    {
+        pinv.push({a[i]*B*B+b[i]*B,i});
+    }
+    
+    cout<<pinv.top().f;
+    return 0;
+}
